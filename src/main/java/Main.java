90c6bfcb67e7a43f0edd8d3fc69adc2405242821
Evgeni Kirichenko <avikon3412@yandex.ru>
2022-11-21 13:03:33 +0300

    создал

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Main {
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        String body;
+        String url = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+        CloseableHttpResponse response = null;
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        HttpGet request = new HttpGet(url);
+        try {
+            response = httpClient.execute(request);
+            body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+            List<Cat> list = mapper.readValue(body, new TypeReference<List<Cat>>() {
+            });
+            list.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                    .forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
